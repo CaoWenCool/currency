0a501d4afe50a7d63ec48d3dd09c127c68fc7ebb
CaoWenCool <1042879511@qq.com>
2022-06-05 21:06:05 +0800

    初始化价格

--- src/main/java/com/currency/qrcode/currency/service/CoinService.java
+++ src/main/java/com/currency/qrcode/currency/service/CoinService.java
@@ -1,34 +1,15 @@
 package com.currency.qrcode.currency.service;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.currency.qrcode.currency.daemon.task.BtcTask;
 import com.currency.qrcode.currency.daemon.task.EthTask;
-import com.currency.qrcode.currency.model.request.ListingLatestRequest;
 import com.currency.qrcode.currency.model.response.AwardInfoResponse;
 import com.currency.qrcode.currency.model.response.CoinPriceResponse;
 import com.currency.qrcode.currency.model.response.EthAddressResponse;
-import com.currency.qrcode.currency.util.HttpsUtils;
-import com.sun.org.apache.regexp.internal.RE;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.URISyntaxException;
-import java.util.Map;
 
 @Service
 public class CoinService {
